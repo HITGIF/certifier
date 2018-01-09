1efc141d3aa40841533617f75d9e7b87ee8e23a1
CarbonylGroup <carbonylgp@gmail.com>
2018-01-09 15:35:06 +0800

    store generated pdf in infinispan cache

--- src/main/java/org/jbossoutreach/certifier/Constants.java
+++ src/main/java/org/jbossoutreach/certifier/Constants.java
@@ -3,5 +3,7 @@ package org.jbossoutreach.certifier;
 
 public class Constants {
     public static final String KEY = "key";
+    public static final String SERVER_URL = "http://localhost:4000";
+    public static final String CERTIFICATE_PATH = "/fetchCert/";
 
 }
--- src/main/java/org/jbossoutreach/certifier/route/GenerateCertRoute.java
+++ src/main/java/org/jbossoutreach/certifier/route/GenerateCertRoute.java
@@ -3,22 +3,33 @@ package org.jbossoutreach.certifier.route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+
+import org.jbossoutreach.certifier.Constants;
 import org.jbossoutreach.certifier.model.Certificate;
 import org.jbossoutreach.certifier.model.Student;
 import org.jbossoutreach.certifier.service.CertManager;
 
 public class GenerateCertRoute implements Route {
     private final CertManager certManager;
+    private DefaultCacheManager cacheManager;
+    private Cache<String, String> cache;
 
     public GenerateCertRoute(CertManager certManager) {
         this.certManager = certManager;
+
+        cacheManager = new DefaultCacheManager();
+        cacheManager.defineConfiguration("certificateCache", new ConfigurationBuilder().build());
+        cache = cacheManager.getCache("certificateCache");
     }
 
     @Override
     public void setup(Router router) {
-
         router.route().handler(BodyHandler.create());
         router.post("/generateCert").handler(this::generateCert);
+        router.get("/fetchCert/*").handler(this::fetchCert);
     }
 
     private void generateCert(RoutingContext routingContext) {
@@ -41,10 +52,28 @@ public class GenerateCertRoute implements Route {
                     .setStatusCode(500)
                     .end("Failed to generate the certificate.");
         } else {
+
+            //Save the generated project in infinispan cache and return the url to fetch it.
+            //Example URL: http://localhost:4000/fetchCert/johndoe@example.com.pdf
+            final String fileURL = Constants.CERTIFICATE_PATH + student.getEmail() + ".pdf";
+            this.cache.put(fileURL, outPath);
+
             routingContext.response()
                     .setStatusCode(201)
-                    .sendFile(outPath);
+                    .end(Constants.SERVER_URL + fileURL);
         }
     }
 
+    private void fetchCert(RoutingContext routingContext) {
+        String certFile = cache.get(routingContext.request().path());
+        if (certFile == null) {
+            routingContext.response()
+                    .setStatusCode(500)
+                    .end("Failed to fetch the certificate.");
+        } else {
+            routingContext.response()
+                    .setStatusCode(201)
+                    .sendFile(certFile);
+        }
+    }
 }
